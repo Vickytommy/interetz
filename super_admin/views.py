7d6b2890063e003c43dd8b338a11ce1ad2aef0eb
Victor Odesola <vickytommy785@gmail.com>
2025-01-21 14:53:59 +0100

    Correct indentation caused by last commit

--- super_admin/views.py
+++ super_admin/views.py
@@ -672,8 +672,8 @@ def add_collection(request):
                                 # Handle invalid file names that cannot be converted to an integer
                                 print(f"Invalid file name: {uploaded_file.name}. Expected a number as the file name.")
                                 
-                        return JsonResponse({'msg': f"{num_of_valid_uploads} of {len(uploaded_files)} {'image' if len(uploaded_files) == 1 else 'images'} processed successfully.", 'success': 1})
-                    else:
+                    return JsonResponse({'msg': f"{num_of_valid_uploads} of {len(uploaded_files)} {'image' if len(uploaded_files) == 1 else 'images'} processed successfully.", 'success': 1})
+                else:
                     return JsonResponse({'msg': 'Uploaded files list is empty.', 'success': 0})
             
             else:
