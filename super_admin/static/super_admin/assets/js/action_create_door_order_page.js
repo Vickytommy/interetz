8d6e010abb2041111e837b6560d4d03b8d267bcd
Victor Odesola <vickytommy785@gmail.com>
2025-03-07 04:21:23 +0100

    Update translation in create door order

--- super_admin/static/super_admin/assets/js/action_create_door_order_page.js
+++ super_admin/static/super_admin/assets/js/action_create_door_order_page.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
 
                                         <div class="md:col-span-2">
                                             <div>
-                                                <label for="" class="block font-medium text-gray-700 text-13 mb-2 dark:text-zink-200">${window.page.bo}</label>
+                                                <label for="" class="block font-medium text-gray-700 text-13 mb-2 dark:text-zink-200">${window.page.drawerBo}</label>
                                                 <input type="number" class="drawer-input w-full border py-2 px-3 text-13 rounded border-gray-400 placeholder:text-13 focus:border focus:border-gray-400 focus:ring-0 focus:outline-none text-gray-700 dark:bg-transparent placeholder:text-gray-600 dark:border-zink-50 dark:placeholder:text-zink-200" required name="bo" id="bo_${counter}">
                                             </div>
                                         </div>
@@ -3255,7 +3255,7 @@ $(document).ready(function(){
         ctx.lineTo(marginLeft + width, (marginTop + height + edgePadding));
         ctx.stroke();
         ctx.fillStyle = "red"; // For the label
-        ctx.fillText(`מ”מ ${width / scaleFactor} רוחב`, marginLeft + width / 2, marginTop + height + edgePadding + 10);
+        ctx.fillText(`מ”מ ${width / scaleFactor} רוחב`, marginLeft + width / 2, marginTop + height + edgePadding + 15);
 
         // BLUE LEFT LINE & LABEL
         ctx.strokeStyle = "blue";
